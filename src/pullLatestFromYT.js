70139bbc1a34549035e3a55e8aefd5c745121a7b
Sam Meech-Ward <sam@meech-ward.me>
2022-10-18 10:57:45 -0700

    add a script to pull latest video from youtube

---
+++ src/pullLatestFromYT.js
@@ -0,0 +1,115 @@
+import {google} from 'googleapis'
+import axios from 'axios'
+
+import path from 'path'
+import fs from 'fs'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+const __dirname = path.resolve()
+
+const youtube = google.youtube({
+  version: 'v3',
+  auth: process.env.YOUTUBE_API_KEY
+});
+
+const mdReplacer = {
+  links (md) {
+    function replacer(match, p1, offset, string) {
+      return `[${p1}](${p1})` 
+    }
+    const regex = /(http.*?(?=\s))/g
+    return md.replace(regex, replacer)
+  }
+}
+
+function convertVideoToMarkdown(video) {
+  const content = video.snippet.description
+  let md = `---
+type: video
+yt_id: ${video.id}
+videoId: ${video.contentDetails.videoId}
+title: "${video.title}"
+date: "${video.snippet.publishedAt}"
+slug: "${video.slug}"
+image: 
+  name: "${video.image.name}"
+  alt: "${video.image.alt}"
+  width: ${video.image.width}
+  height: ${video.image.height}
+status: 'published'
+description: "${video.snippet.description.split('\n')[0]}"
+tags: []
+---
+
+${mdReplacer.links(content).split("🔗Moar Links")[0].split('\n').map(line => {
+  if (line.startsWith('#') || !isNaN(+line[0])) {
+    return `* ${line}`
+  } 
+  if (line.toLowerCase().startsWith('chapters')) {
+    return `## Chapters:\n`
+  }
+  return line
+}).join('\n')}
+`
+
+return md
+}
+
+
+async function createFileForVideo(video) {
+  const slug = video.snippet.title.toLowerCase().replace(/[\+\?#\.\|\-\_,]/g, '').replace(/\s+/g, '-')
+  video.slug = slug
+  video.title = video.snippet.title
+
+  // Setup directories
+  const contentDir = path.resolve(__dirname, 'content')
+  const videosDir = path.resolve(contentDir, `videos`)
+  const videoDir = path.resolve(videosDir, video.slug)
+  const videoImagesDir = path.resolve(videoDir, 'images')
+  fs.mkdirSync(videoImagesDir, { recursive: true })
+
+
+  // handle main image
+  let thumbnail 
+  for (const kind of ["maxres", "standard", "high", "medium", "default"]) {
+    if (video.snippet.thumbnails[kind]) {
+      thumbnail = video.snippet.thumbnails[kind]
+      break
+    }
+  }
+  const imageExtension = thumbnail?.url?.split('.').pop()
+  const image = {
+    name: `${slug}.${imageExtension}`,
+    alt: video.snippet.title,
+    width: thumbnail?.width || null,
+    height: thumbnail?.height || null
+  }
+  video.image = image
+  if (thumbnail) {
+    const mainImageResult = await axios.get(thumbnail.url, { responseType: 'arraybuffer' })
+    fs.writeFileSync(path.resolve(videoImagesDir, image.name), mainImageResult.data)
+  }
+
+  // handle markdown
+  const md = convertVideoToMarkdown(video)
+  fs.writeFileSync(path.resolve(videoDir, 'index.mdx'), md)
+}
+
+
+const uploadsId = await youtube.channels.list({
+  part: 'contentDetails',
+  // forUsername: 'sammeechward'
+  id: 'UC6aTLuI_j4-0wiDSzmaPctQ'
+}).then((res) => {
+  return res.data.items[0].contentDetails.relatedPlaylists.uploads
+})
+
+
+const mostRecentUpload = await youtube.playlistItems.list({
+  part: 'snippet,contentDetails',
+  maxResults: 1,
+  playlistId: uploadsId
+})
+await Promise.all(mostRecentUpload.data.items.map(createFileForVideo))
