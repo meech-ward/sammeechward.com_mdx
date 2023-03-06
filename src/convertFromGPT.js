76b341f4320598f99afe5d9f48fa15f1a6dcf918
Sam Meech-Ward <sam@sam.local>
2023-03-05 19:12:17 -0800

    Update image

--- src/convertFromGPT.js
+++ src/convertFromGPT.js
@@ -3,7 +3,8 @@ import path from 'path'
 import parseMarkdown from 'front-matter-markdown'
 import sharp from 'sharp'
 
-import json from '../content/articles/dotnet/entitiy-framework-timestamps/chatGPT.json' assert { type: "json" }
+// import json from '../content/articles/dotnet/entitiy-framework-timestamps/chatGPT.json' assert { type: "json" }
+import json from './tmp.json' assert { type: "json" }
 
 const title = json.title
 const timestamp = json.create_time
